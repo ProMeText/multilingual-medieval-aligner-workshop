59d6a27b2a673411c59c25f9ba568b61eddafafb
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2023-12-25 21:23:43 +0100

    fix bug

--- latinAlignmentTests.py
+++ latinAlignmentTests.py
@@ -7,8 +7,9 @@ latin =  "text+berg/test_lat-spo/rome_w.txt"
 
 with open(spanish, "r") as spanish_file:
     spanish = spanish_file.readlines()
+    
 with open(latin, "r") as latin_file:
-    latin_file = spanish_file.readlines()
+    latin = latin_file.readlines()
 
 aligner = Bertalign(latin, spanish)
 aligner.align_sents()
