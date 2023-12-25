9d91240be7bc7255b8381003daa75638a84cf65b
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2023-12-25 22:41:14 +0100

    Add sys option

--- latinAlignmentTests.py
+++ latinAlignmentTests.py
@@ -7,9 +7,6 @@ latin = sys.argv[2]
 with open(spanish, "r") as spanish_file:
     spanish = spanish_file.read()
     
-with open(spanish_2, "r") as spanish_file_2:
-    spanish_2 = spanish_file_2.read()
-    
 with open(latin, "r") as latin_file:
     latin = latin_file.read()
 
