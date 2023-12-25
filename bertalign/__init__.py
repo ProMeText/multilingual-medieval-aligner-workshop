e0dd3983ed4a012d973c268ec945dbf644b677e9
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2023-12-25 19:33:08 +0100

    add model selection

--- bertalign/__init__.py
+++ bertalign/__init__.py
@@ -10,8 +10,13 @@ from bertalign.encoder import Encoder
 # See other cross-lingual embedding models at
 # https://www.sbert.net/docs/pretrained_models.html
 
-model_name = "distiluse-base-multilingual-cased-v2"
-model_name = "LaBSE"
-model = Encoder(model_name)
+
+models = {0: "distiluse-base-multilingual-cased-v2", 1: "LaBSE"}
+as_list = ""
+for key, value in models.items():
+    as_list += f"{int(key)}\n{value}\n"
+    
+nb = input(f'Choose a model: {key}')
+model = Encoder(models[nb])
 
 from bertalign.aligner import Bertalign
