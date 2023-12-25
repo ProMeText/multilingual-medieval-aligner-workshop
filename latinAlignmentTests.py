18011be22a09bdb4cc0e5b988ba200c03cc6d13d
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2023-12-25 21:59:38 +0100

    fix path issue

--- latinAlignmentTests.py
+++ latinAlignmentTests.py
@@ -2,8 +2,8 @@ from bertalign import Bertalign
 
 
 
-spanish = "text+berg/latin_castillan/Val_S_1_2_5.txt"
-latin =  "text+berg/latin_castillan/Rome_W_1_2_5.txt"
+spanish = "text+berg/latin_castilian/Val_S_1_2_5.txt"
+latin =  "text+berg/latin_castilian/Rome_W_1_2_5.txt"
 
 with open(spanish, "r") as spanish_file:
     spanish = spanish_file.read()
