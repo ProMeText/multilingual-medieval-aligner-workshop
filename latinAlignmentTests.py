271248690c0416b8990f843bbd36dcf0e4663d82
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2023-12-25 21:22:56 +0100

    Update test script and data

--- latinAlignmentTests.py
+++ latinAlignmentTests.py
@@ -6,9 +6,9 @@ spanish = "text+berg/test_lat-spo/sev_z.txt"
 latin =  "text+berg/test_lat-spo/rome_w.txt"
 
 with open(spanish, "r") as spanish_file:
-    spanish = spanish_file.read()
+    spanish = spanish_file.readlines()
 with open(latin, "r") as latin_file:
-    latin_file = spanish_file.read()
+    latin_file = spanish_file.readlines()
 
 aligner = Bertalign(latin, spanish)
 aligner.align_sents()
