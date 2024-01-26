165fd213a9d3863b5e3b8142e189d6e34f2bd68d
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2024-01-26 17:21:36 +0100

    Update latinAlignmet test

--- latinAlignmentTests.py
+++ latinAlignmentTests.py
@@ -5,13 +5,12 @@ spanish = sys.argv[1]
 latin = sys.argv[2]
 
 with open(spanish, "r") as spanish_file:
-    spanish = spanish_file.read()
+    spanish = spanish_file.read().split("\n")
     
 with open(latin, "r") as latin_file:
-    latin = latin_file.read()
+    latin = latin_file.read().split("\n")
 
 aligner = Bertalign(latin, spanish)
 aligner.align_sents()
 print(aligner.result)
-exit(0)
 aligner.print_sents()
