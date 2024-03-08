dd0c3f99de7a12c845f6656453c721f56a61aed1
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2024-03-08 19:49:52 +0100

    Add device selection

--- bertalign/encoder.py
+++ bertalign/encoder.py
@@ -1,11 +1,12 @@
 import numpy as np
-
+import torch
 from sentence_transformers import SentenceTransformer
 from bertalign.utils import yield_overlaps
 
 class Encoder:
     def __init__(self, model_name):
-        self.model = SentenceTransformer(model_name)
+        device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        self.model = SentenceTransformer(model_name_or_path=model_name, device=device)
         self.model_name = model_name
 
     def transform(self, sents, num_overlaps):
