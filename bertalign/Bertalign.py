b1f214e41cc4aef4b45fa836019fde81e29d44df
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2024-03-22 16:42:41 +0100

    Change filename

--- bertalign/Bertalign.py
+++ bertalign/Bertalign.py
@@ -6,10 +6,9 @@ __author__ = "Jason (bfsujason@163.com)"
 __version__ = "1.1.0"
 
 from bertalign.encoder import Encoder
-
 # See other cross-lingual embedding models at
 # https://www.sbert.net/docs/pretrained_models.html
-
+print("Chargement 1")
 
 models = {0: "distiluse-base-multilingual-cased-v2", 1: "LaBSE"}
 # as_list = ""
