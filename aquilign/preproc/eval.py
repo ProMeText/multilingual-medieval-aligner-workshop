4211f6d0ec953fd56710b1f94d5fdc446f888cb2
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2024-05-30 11:06:29 +0200

    Keep on with eval function

--- aquilign/preproc/eval.py
+++ aquilign/preproc/eval.py
@@ -57,6 +57,8 @@ def test(file, model_path, tokenizer_name, num):
         print(f"Text: {txt_example}")
         print(f"Predicted: {bert_labels}")
         print(f"Ground Truth: {cropped_gt_labels}")
+        print(len(bert_labels))
+        print(len(cropped_gt_labels))
         print("---")
        
         
