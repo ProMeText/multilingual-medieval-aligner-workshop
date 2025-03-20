810a8da558cc5088f8bd0a19abc96c8b883a1eca
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2025-03-20 13:06:50 +0100

    Adapt best model

--- train_tokenizer.py
+++ train_tokenizer.py
@@ -111,6 +111,7 @@ def training_trainer(modelName, train_dataset, dev_dataset, eval_dataset, num_tr
     # print the whole log_history with the compute metrics
     best_precision_step, best_step_metrics = utils.get_best_step(trainer.state.log_history)
     print(trainer.state.log_history)
+    print(best_precision_step, best_step_metrics)
     exit(0)
 
     # On s'assure de prendre le step le plus proche
