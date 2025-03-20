a54187185cf2cb14cb3653adb379013b76132ebc
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2025-03-20 13:22:35 +0100

    Change logging strategy

--- train_tokenizer.py
+++ train_tokenizer.py
@@ -72,7 +72,8 @@ def training_trainer(modelName, train_dataset, dev_dataset, eval_dataset, num_tr
     training_args = TrainingArguments(
         output_dir=f"results_{out_name}/epoch{num_train_epochs}_bs{batch_size}",
         num_train_epochs=num_train_epochs,
-        logging_steps=logging_steps,
+        logging_strategy="steps",
+        logging_steps=1,
         per_device_train_batch_size=batch_size,
         per_device_eval_batch_size=batch_size,
         evaluation_strategy="epoch",
